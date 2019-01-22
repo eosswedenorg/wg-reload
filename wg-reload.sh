0a4e7ef161b38ea3650a399e60a81e68c6b1cdd2
Henrik Hautakoski <henrik@eossweden.org>
2019-01-22 13:11:35 +0100

    Add support for switching ip.

--- wg-reload.sh
+++ wg-reload.sh
@@ -8,6 +8,7 @@ if [ $# -lt 1 ]; then
 fi
 
 WG=$(which wg)
+IP=$(which ip)
 IFACE=$1
 CONFIG_FILE=/etc/wireguard/${IFACE}.conf
 
@@ -20,9 +21,20 @@ fi
 # and pass the string as input to setconf
 ${WG} setconf ${IFACE} <(sed -E '/^[[:space:]]*(\#|Address|DNS|MTU|PreUp|PostUp|PreUp|PreDown|SaveConfig)/ d' ${CONFIG_FILE})
 
-if [ $? -eq 0 ]; then
-	echo "[OK] ${IFACE} was reloaded successfully"
-else
+if [ $? -ne 0 ]; then
 	echo "[ERROR] could not reload interface because of previous error."
 	exit 1
 fi
+
+# Check if we need to update IP
+NEW_IP=$(sed -En 's/^[[:space:]]*Address[[:space:]]*\=[[:space:]]*([0-9\./]+)/\1/p' ${CONFIG_FILE})
+CURRENT_IP=$(${IP} -o -4 addr show ${IFACE} | awk '{print $4}')
+
+if [ "${NEW_IP}" != "${CURRENT_IP}" ]; then
+	echo "Update to ${NEW_IP}"
+	# NOTE: change,replace does NOT do what you want. have to use add+del
+	$IP addr add ${NEW_IP} dev ${IFACE}
+	$IP addr del ${CURRENT_IP} dev ${IFACE}
+fi
+
+echo "[OK] ${IFACE} was reloaded successfully"
